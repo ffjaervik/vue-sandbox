c3676c9a330f678894099893141c1b22e88f4468
Fabian Deckmann Fjaervik <fabianfjaervik@Fabians-MacBook-Pro.local>
2023-05-01 11:55:41 +0200

    data binding

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,28 @@
 <template>
- 
+  <title>Vue Basics</title>
+  <h1>Vue Basics</h1>
+  <nav>
+    <ul>
+      <li v-for="link in links" :key="link.name" :href="link.url">{{ link.name }}</li>
+    </ul>
+  </nav>
 </template>
 
 <script>
-
-
-export default {
-  name: 'App',
-  
-}
+  export default {
+    name: "App",
+    data() {
+      return {
+        links: [
+          { name: "Home", url: "/" },
+          { name: "About", url: "/about" },
+          { name: "Contact", url: "/contact" },
+          { name: "Login", url: "/login" },
+          { name: "Register", url: "/register" },
+        ],
+      };
+    },
+  };
 </script>
 
-<style>
-
-</style>
+<style></style>
